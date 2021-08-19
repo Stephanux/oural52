965eb016c636a69623d1354ede3df557d295fcd3
thomas <gentreau.thomas@gmail.com>
2021-08-19 14:28:51 +0200

    nouveau controleur pour l'upload multiple

---
+++ routes/updateSQLAndUploadSingle.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+/* POST UPDATE via Sequelize raw query . */
+router.post(('/', upload.single('doc_pdf')), function (req, res, next) {
+    if ((req.session.passport) && (req.session.passport.user != null)) {
+        // ici on réalise une requête d'insertion dans une base SQL
+        var params_name = req.message.params_query;
+        var params_value = [];
+        for (let i = 0; i < params_name.length; i++) {
+            params_value.push(req.body[params_name[i]]);
+        }
+        params_value.push(parseInt(req.query.id)); // ajout de l'id passé dans l'URL pour la clause Where
+        console.log("params_value :", params_value);
+        console.log("req.body : ", req.body);
+
+        // gestion du fichier uploaded via multer.
+        console.log('file : ', req.file); // contient les infos sur le fichier uploadé
+        console.log('body : ', req.body); // contient les autres données du formulaire
+        req.body[req.file.fieldname] = req.file.originalname;
+        fs.rename(req.file.path, req.file.destination + req.file.originalname, () => {
+            console.log("\nFile : " + req.file.originalname + " Uploaded and Renamed!\n ");
+        });
+        
+        global.sequelize.query(req.message.sql_query, {
+            replacements: params_value,
+            type: sequelize.QueryTypes.UPDATE
+        })
+            .then(function (result) { // sql query success
+                console.log('listes retour updateSQL : ', result);
+                res.redirect(req.message.redirect + '?msg=Modification correctement effectuée');
+            }).catch(function (err) { // sql query error
+                console.log('error select', err);
+                res.redirect(req.message.redirect + '?msg=Il y a une erreur');
+                res.send('Erreur : ' + err);
+            });
+    } else {
+        res.redirect('/');  // affichage boîte de login si pas authentifié
+    }
+});
+
+module.exports = router;
