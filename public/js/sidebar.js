4b6a98857327ed5dcd1cdc024531de4764ea240b
tgentreau <gentreau.thomas@gmail.com>
2021-09-04 12:45:37 +0200

    intégration sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,76 +1,82 @@
-const icon = document.querySelector('.menu-icon');
-const sideBar = document.querySelector('#sidebar');
-const closeMenu = document.querySelector('.menu-close')
-const vehicule = document.querySelector('.vehicules');
-const menuVehicule = document.querySelector('.sub-menu-vehicules');
-const piece = document.querySelector('.pieces');
-const menuPiece = document.querySelector('.sub-menu-pieces');
-const stock = document.querySelector('.stocks');
-const menuStock = document.querySelector('.sub-menu-stock');
-const editVehicule = document.querySelector('.list-vehicule');
-const menuEditVehicule = document.querySelector('.sub-menu-list-vehicules');
-const editPiece = document.querySelector('.list-piece');
-const menuEditPiece = document.querySelector('.sub-menu-list-piece');
+window.onload = ()=>{
 
-icon.addEventListener('click', show);
-
-//SHOW SIDEBAR//
-function show(){
-  sideBar.classList.toggle('active');
-  vehicule.style.display = "block";
-  piece.style.display = "block";
-  stock.style.display = "block";
-  editVehicule.style.display = "block";
-  editPiece.style.display = "block";
+    let icon = document.querySelector('.menu-icon');
+    let sideBar = document.querySelector('#sidebar');
+    let closeMenu = document.querySelector('.menu-close')
+    let vehicule = document.querySelector('.vehicules');
+    let menuVehicule = document.querySelector('.sub-menu-vehicules');
+    let piece = document.querySelector('.pieces');
+    let menuPiece = document.querySelector('.sub-menu-pieces');
+    let stock = document.querySelector('.stocks');
+    let menuStock = document.querySelector('.sub-menu-stock');
+    let editVehicule = document.querySelector('.list-vehicule');
+    let menuEditVehicule = document.querySelector('.sub-menu-list-vehicules');
+    let editPiece = document.querySelector('.list-piece');
+    let menuEditPiece = document.querySelector('.sub-menu-list-piece');
+    
+    console.log(icon);
+    console.log(sideBar);
+    
+    //SHOW SIDEBAR//
+    icon.addEventListener('click', ()=>{
+        console.log('toto')
+        sideBar.classList.toggle('active');
+        vehicule.style.display = "block";
+        piece.style.display = "block";
+        stock.style.display = "block";
+        editVehicule.style.display = "block";
+        editPiece.style.display = "block";
+    });
+    
+    //SHOW SUB-MENUS//
+    vehicule.addEventListener('click', () => {
+        menuVehicule.style.display = "block";
+        menuPiece.style.display = "none";
+        menuStock.style.display = "none";
+        menuEditVehicule.style.display = "none";
+        menuEditPiece.style.display = "none";
+    })
+    
+    piece.addEventListener('click', () => {
+        menuVehicule.style.display = "none";
+        menuPiece.style.display = "block";
+        menuStock.style.display = "none";
+        menuEditVehicule.style.display = "none";
+        menuEditPiece.style.display = "none";
+    })
+    
+    stock.addEventListener('click', () => {
+        menuVehicule.style.display = "none";
+        menuPiece.style.display = "none";
+        menuStock.style.display = "block";
+        menuEditVehicule.style.display = "none";
+        menuEditPiece.style.display = "none";
+    })
+    
+    editVehicule.addEventListener('click', () => {
+        menuVehicule.style.display = "none";
+        menuPiece.style.display = "none";
+        menuStock.style.display = "none";
+        menuEditVehicule.style.display = "block";
+        menuEditPiece.style.display = "none";
+    })
+    
+    editPiece.addEventListener('click', () => {
+        menuVehicule.style.display = "none";
+        menuPiece.style.display = "none";
+        menuStock.style.display = "none";
+        menuEditVehicule.style.display = "none";
+        menuEditPiece.style.display = "block";
+    })
+    
+    //CLOSE SIDEBAR AND SUB-MENUS//
+    closeMenu.addEventListener('click', () => {
+        sideBar.classList.remove('active');
+        menuVehicule.style.display = "none";
+        menuPiece.style.display = "none";
+        menuStock.style.display = "none";
+        menuEditVehicule.style.display = "none";
+        menuEditPiece.style.display = "none";
+    });
+    
 }
-
-//SHOW SUB-MENUS//
-vehicule.addEventListener('click', () => {
-    menuVehicule.style.display = "block";
-    menuPiece.style.display = "none";
-    menuStock.style.display = "none";
-    menuEditVehicule.style.display = "none";
-    menuEditPiece.style.display = "none";
-})
-
-piece.addEventListener('click', () => {
-    menuVehicule.style.display = "none";
-    menuPiece.style.display = "block";
-    menuStock.style.display = "none";
-    menuEditVehicule.style.display = "none";
-    menuEditPiece.style.display = "none";
-})
-
-stock.addEventListener('click', () => {
-    menuVehicule.style.display = "none";
-    menuPiece.style.display = "none";
-    menuStock.style.display = "block";
-    menuEditVehicule.style.display = "none";
-    menuEditPiece.style.display = "none";
-})
-
-editVehicule.addEventListener('click', () => {
-    menuVehicule.style.display = "none";
-    menuPiece.style.display = "none";
-    menuStock.style.display = "none";
-    menuEditVehicule.style.display = "block";
-    menuEditPiece.style.display = "none";
-})
-
-editPiece.addEventListener('click', () => {
-    menuVehicule.style.display = "none";
-    menuPiece.style.display = "none";
-    menuStock.style.display = "none";
-    menuEditVehicule.style.display = "none";
-    menuEditPiece.style.display = "block";
-})
-
-//CLOSE SIDEBAR AND SUB-MENUS//
-closeMenu.addEventListener('click', () => {
-    sideBar.classList.remove('active');
-    menuVehicule.style.display = "none";
-    menuPiece.style.display = "none";
-    menuStock.style.display = "none";
-    menuEditVehicule.style.display = "none";
-    menuEditPiece.style.display = "none";
-});
