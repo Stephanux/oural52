b0aab9cdcd1131acaf36c01b8c7752f8a58d019b
thomas <gentreau.thomas@gmail.com>
2021-09-20 16:51:50 +0200

    supression des liens a dans les buttons et ajout d'une redirection au clic dans main.js

--- public/js/main.js
+++ public/js/main.js
@@ -201,6 +201,28 @@ window.onload = () => {
     for (i = 0; i < date.length; i++) {
         date[i].innerHTML = dateParse(date[i].innerText);
     }
+    /**REDIRECTION MAIN PAGE**/
+
+    let stockV = document.querySelector('.stockV');
+    let stockP = document.querySelector('.stockP');
+    let addV = document.querySelector('.addV');
+    let addP = document.querySelector('.addP');
+
+    stockV.addEventListener('click', () => {
+        window.location.href = "/stockVehicule";
+    });
+
+    stockP.addEventListener('click', () => {
+        window.location.href = "/stockPiece";
+    });
+
+    addV.addEventListener('click', () => {
+        window.location.href = "/formInsertVehicule";
+    });
+
+    addP.addEventListener('click', () => {
+        window.location.href = "/formInsertPiece";
+    });
 
     /**GESTION MSG**/
 
