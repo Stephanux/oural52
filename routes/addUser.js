f946b7d0813e7f425476e5281f729097430c8554
thomas <gentreau.thomas@gmail.com>
2021-09-22 14:15:29 +0200

    ajot de bcryptjs pour le hashage/salage des mdp

--- routes/addUser.js
+++ routes/addUser.js
@@ -1,32 +1,28 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var ObjectId = mongoose.Types.ObjectId;
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 const bcrypt = require('bcryptjs');
 
 router.route('/').post((req, res) => {
     if ((req.session.passport) && (req.session.passport.user != null)) {
-        const pass = req.body.mdp;
-        console.log('mot de passe: ', pass);
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(pass, salt, (err, hash) => {
-                // On doit créer via Mongoose un _id pour faire l'insertion dans la base
-                if (!req.body._id) req.body._id = new ObjectId();
-                global.schemas[req.message.modelName].create([req.body], (err, result) => {
-                    if (err) {
-                        console.log('erreur :' , err);
-                        res.redirect(req.message.redirect + "?msg=Il y a une erreur")
-                    };
+        let pass = req.body.mdp;
+        pass = bcrypt.hashSync(pass, 10);
+        // On doit créer via Mongoose un _id pour faire l'insertion dans la base
+        if (!req.body._id) req.body._id = new ObjectId();
+        global.schemas[req.message.modelName].create([req.body], (err, result) => {
+            if (err) {
+                console.log('erreur :' , err);
+                res.redirect(req.message.redirect + "?msg=Il y a une erreur")
+            };
         
-                    console.log('insertOne mongoose: ', result);
-                    // on refait une requête pour récupérer tous les enregs du modelName
-                    global.schemas[req.message.modelName].find({}, (err, result2) => {
-                        console.log("result into deleteUser : ", result2);
-                        res.redirect(req.message.redirect + "?msg=Ajout correctement effectué")
-                    });          
-                }); // fin de l'insert()
-            });
-        });
+            console.log('insertOne mongoose: ', result);
+            // on refait une requête pour récupérer tous les enregs du modelName
+            global.schemas[req.message.modelName].find({}, (err, result2) => {
+                console.log("result into deleteUser : ", result2);
+                res.redirect(req.message.redirect + "?msg=Ajout correctement effectué")
+            });          
+        }); // fin de l'insert()
     } else {
         res.redirect('/');
     }
