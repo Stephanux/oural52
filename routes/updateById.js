1ed5e6127eca858ea255f936a1c744fae80eae87
tgentreau <gentreau.thomas@gmail.com>
2021-10-26 21:15:18 +0200

    crud côté utilisateur fonctionnel

--- routes/updateById.js
+++ routes/updateById.js
@@ -6,16 +6,20 @@ const ObjectId = mongoose.Types.ObjectId;
 /* Modification d'un utilisateur dans la base de données */
 router.post(('/'), (req, res) => {
     if ((req.session.passport) && (req.session.passport.user != null)) {
-        global.schemas[req.message.modelName].updateOne({ _id = new ObjectId(req.body._id) }, { $set = req.body }), function (err, result) {
-                if (err) { throw err; }
+        global.schemas[req.message.modelName].updateOne({ _id: new ObjectId(req.body._id) }, { $set: req.body }, function (err, result) {
+                if (err) { 
+                    throw err 
+                }
 
                 console.log('from updateById: ', result);
                 global.schemas[req.message.modelName].find({ _id: new ObjectId(req.body._id) }, (err, result) => {
-                    if (err) { throw err; }
+                    if (err) { 
+                        throw err 
+                    }
                     console.log('users: ', result);
-                    res.redirect(req.message.redirect + "&msg=Modification correctement effectué");
+                    res.redirect(req.message.redirect + "?msg=Modification correctement effectué");
                 });
-        }; // fin de l'update()
+        }); // fin de l'update()
     } else {
         res.redirect('/');  // affichage boîte de login si pas authentifié
     }
